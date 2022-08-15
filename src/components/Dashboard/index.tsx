371f09fb5a3ad5de173090311f2b4eea2c1dd6fa
AugustoTI <augustocesarti2021@gmail.com>
2022-08-15 16:10:52 -0300

    creating the transaction table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from '../Summary';
+import { TransactionTable } from '../TransactionTable';
 import * as S from './styles';
 
 export const Dashboard = () => {
   return (
     <S.Container>
       <Summary />
+      <TransactionTable />
     </S.Container>
   );
 };
