c8aaeb8716d963798fa2e8475385bc3b80332156
AugustoTI <augustocesarti2021@gmail.com>
2022-08-15 18:33:49 -0300

    adding page modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,32 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyles } from './styles/global';
-import { createServer } from 'miragejs';
+import Modal from 'react-modal';
+import { useState } from 'react';
 
-createServer({
-  routes() {
-    this.namespace = 'api';
-
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 1000,
-          type: 'deposit',
-          category: 'Freelancer',
-          createAt: new Date(),
-        },
-        {
-          id: 2,
-          title: 'Transaction 2',
-          amount: 500,
-          type: 'withdraw',
-          category: 'House',
-          createAt: new Date(),
-        },
-        {
-          id: 3,
-          title: 'Transaction 3',
-          amount: 60,
-          type: 'deposit',
-          category: 'Food',
-          createAt: new Date(),
-        },
-      ];
-    });
-  },
-});
+Modal.setAppElement('#root');
 
 export const App = () => {
+  const [newTransactionModalOpen, setNewTransactionModalOpen] = useState(false);
+
+  const handleOpenNewTransactionModal = () => {
+    setNewTransactionModalOpen(true);
+  };
+
+  const handleCloseNewTransactionModal = () => {
+    setNewTransactionModalOpen(false);
+  };
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={newTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar</h2>
+      </Modal>
       <GlobalStyles />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 1000,
+          type: 'deposit',
+          category: 'Freelancer',
+          createAt: new Date(),
+        },
+        {
+          id: 2,
+          title: 'Transaction 2',
+          amount: 500,
+          type: 'withdraw',
+          category: 'House',
+          createAt: new Date(),
+        },
+        {
+          id: 3,
+          title: 'Transaction 3',
+          amount: 60,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
